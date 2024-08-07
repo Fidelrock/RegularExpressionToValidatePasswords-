cba42cbf393e7f31fc0670171e6e42866d3e7130
fidelrock <fidelisodhiambo254@gmail.com>
2024-08-07 09:35:02 +0300

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PasswordValidation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Creating a regular expression pattern that checks if a password meets the requirements
+            string pattern1 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@_$])[a-zA-Z0-9!#@_$]{8,}$";
+
+            // Sample password to validate
+            string inputToValidate = "Fidelrock@1234";
+
+            // Validate the password using the regular expression
+            if (Regex.IsMatch(inputToValidate, pattern1))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
